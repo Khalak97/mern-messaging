8516bb9c9b6fd33ac879e0ba8ed9278d419e891c
Khalak97 <valodyakh@gmail.com>
2020-06-02 12:46:18 +0300

    add login and logout dynamicly

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,11 +2,17 @@ import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
 
+import './Chat.css';
+
+import InfoBar from '../InfoBar/InfoBar';
+
 let socket;
 
 const Chat = ({ location }) => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
     const ENDPOINT = 'localhost:5000';
 
     useEffect(() => {
@@ -20,14 +26,35 @@ const Chat = ({ location }) => {
         socket.emit('join', { name, room }, () => {});
 
         return () => {
-            socket.emit('disconnected');
+            socket.emit('disconnect');
 
             socket.off();
         }
     }, [ENDPOINT, location.search]);
 
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages([...messages, message]);
+        })
+    }, [messages]);
+
+    // function for sending messages
+    const sendMessage = (event) => {
+        event.preventDefault();
+        
+        if(message) {
+            socket.emit('sendMessage', message, () => setMessage(''));
+        }
+    }
+
+    console.log(message, messages);
+
     return (
-        <h1>Chat</h1>
+        <div className="outerContainer">
+            <div className="container">
+                <InfoBar room={room} />
+            </div>
+        </div>
     )
 }
 
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
+const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-const http = require('http');
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
 
@@ -12,9 +12,9 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on('connection', (socket) => {
+io.on('connect', (socket) => {
     socket.on('join', ({ name, room }, callback) => {
-        const { error, user } = addUser({ id: socked.id, name, room });
+        const { error, user } = addUser({ id: socket.id, name, room });
 
         if(error) return callback(error);
 
@@ -22,6 +22,16 @@ io.on('connection', (socket) => {
         socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined!`});
 
         socket.join(user.room);
+
+        callback();
+    });
+
+    socket.on('sendMassage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', { user: user.name, text: message });
+
+        callback();
     });
 
     socket.on('disconnect', () => {
